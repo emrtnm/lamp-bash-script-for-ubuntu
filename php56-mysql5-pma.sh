731d2500e783e67e3874373a8db77fb553c1ba56
Emrullah Tanıma <emrtnm@gmail.com>
2022-08-10 16:57:04 +0300

    Bug fixed.

--- php56-mysql5-pma.sh
+++ php56-mysql5-pma.sh
@@ -34,10 +34,15 @@ printf $DIVIDER
 printf "Mysql is installing...\n"
 printf $DIVIDER
 
+echo Enter a new mysql password:
+read mysqlpasswd
+
 sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server -y
+
+sudo mysql -Bse "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysqlpasswd';"
 
 mysql_secure_installation
 
