c3aa66932fda17d3ec943f592c7bfdc6c379c00e
Emrullah Tanıma <info@emrullahtanima.com>
2022-11-13 23:15:59 +0300

    bug fixed.

--- vhost.sh
+++ vhost.sh
@@ -4,9 +4,9 @@ DIVIDER="\n########################################\n\n"
 echo "Enter a domain name: \c"
 read domainname
 
-sudo mkdir /var/www/$domainname
-sudo chown -R $USER:$USER /var/www/$domainname
-sudo chmod -R 775 /var/www/$domainname
+sudo mkdir /var/www/${domainname}
+sudo chown -R $USER:$USER /var/www/${domainname}
+sudo chmod -R 775 /var/www/${domainname}
 sudo sed -i "s|your_domain|${domainname}|g' domainhost.conf
 sudo cat domainhost.conf >> /etc/apache2/sites-available/$domainname.conf
 sudo sed -i "s|${domainname}|your_domain|g" domainhost.conf
