835dee327189bf195fea4f803ab86727a8eeb37d
Emrullah Tanıma <emrtnm@gmail.com>
2022-11-16 14:14:06 +0300

    Edited: add-host package.

--- add-host.sh
+++ add-host.sh
@@ -6,8 +6,7 @@ read domainname
 
 sudo mkdir /var/www/${domainname}
 sudo chown -R www-data:www-data /var/www/${domainname}
-sudo find /var/www/${domainname} -type d -print0 | xargs -0 chmod 755
-sudo find /var/www/${domainname} -type f -print0 | xargs -0 chmod 644
+sudo chmod -R 755 /var/www/${domainname}
 
 sudo sed -i "s|your_domain|${domainname}|g" domainhost.conf
 sudo cat domainhost.conf >> /etc/apache2/sites-available/$domainname.conf
